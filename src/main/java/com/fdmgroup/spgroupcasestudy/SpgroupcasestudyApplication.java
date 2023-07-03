06bed9b5de1bf4c06f90f759a609c6c554035d8a
ryan.tan1 <ryan.tan1@fdmgroup.com>
2023-07-03 10:10:28 +0800

    Made blank project

---
+++ src/main/java/com/fdmgroup/spgroupcasestudy/SpgroupcasestudyApplication.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.spgroupcasestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpgroupcasestudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpgroupcasestudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fdmgroup/spgroupcasestudy/SpgroupcasestudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.spgroupcasestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpgroupcasestudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
